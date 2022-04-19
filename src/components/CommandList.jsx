055afea38b1027c57369443a6ceb335d0198145a
Кирилл Колчанов <me@iMac-Kirill.local>
2022-04-19 10:06:17 +0500

    master_react | Стилизация. Зафиксировал header, увеличил размер шрифта, отступы для элемента li

--- src/components/CommandList.jsx
+++ src/components/CommandList.jsx
@@ -8,15 +8,17 @@ const CommandList = ({ searchValue }) => {
       c.command?.toLowerCase().includes(searchValue.toLowerCase())
   );
   return (
-    <div className="container">
-      <ul>
-        {array.map((c, i) => (
-          <li key={i}>
-            <b>{c.command}</b> <span>{c.about}</span>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <main>
+      <div className="container">
+        <ul>
+          {array.map((c, i) => (
+            <li key={i}>
+              <b>{c.command}</b> <span>{c.about}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </main>
   )
 }
 
